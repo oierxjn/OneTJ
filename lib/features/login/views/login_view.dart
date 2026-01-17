d581cac7b248b7ff6aace405070c1fa31bc0fbed
oierxjn <1934672944@qq.com>
2026-01-17 19:50:43 +0800

    Build: 替换webview_windows为flutter_inappwebview并移除相关代码
    移除assets/packages/flutter_inappwebview目录及所有相关代码
    更新pubspec.yaml使用本地flutter_inappwebview包
    修改LoginView使用InAppWebView替代AdaptiveWebView
    删除AdaptiveWebView组件及相关windows适配代码
    更新windows平台插件配置使用flutter_inappwebview_windows

--- lib/features/login/views/login_view.dart
+++ lib/features/login/views/login_view.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:onetj/widgets/adaptive_webview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final url = Uri.parse('https://www.baidu.com');
+    final url = 'https://www.baidu.com';
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('一统同济'),
       ),
-      body: AdaptiveWebView(initialUrl: url),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: WebUri(url)),
+      ),
     );
   }
 }
