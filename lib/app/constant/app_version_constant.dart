3d17eac94ecd7708bbb622ba6cf630a7bf58df70
oierxjn <1934672944@qq.com>
2026-03-29 13:56:32 +0800

    Chore: 更新应用版本至2.4.1
    更新安装脚本、应用配置文件和版本常量，同步更新日志和pubspec版本号

--- lib/app/constant/app_version_constant.dart
+++ lib/app/constant/app_version_constant.dart
@@ -1,3 +1,3 @@
 const String oneTJAppName = 'OneTJ';
-const String oneTJAppVersion = '2.3.0';
-const String oneTJAppBuildNumber = '12';
+const String oneTJAppVersion = '2.4.1';
+const String oneTJAppBuildNumber = '13';
